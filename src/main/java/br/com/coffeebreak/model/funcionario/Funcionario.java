f255a036d83e269242f4d34673c7a9eccc513744
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-04-16 02:15:03 -0300

    feat: model funcionario

---
+++ src/main/java/br/com/coffeebreak/model/funcionario/Funcionario.java
@@ -0,0 +1,35 @@
+package br.com.coffeebreak.model.funcionario;
+
+import br.com.coffeebreak.enums.TipoFuncionario;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "funcionarios")
+public class Funcionario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private TipoFuncionario tipoFuncionario;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+
+}
