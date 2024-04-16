a15993e789250d3e300bdb89eeb85d3e09e492cb
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-04-16 02:14:52 -0300

    feat: model estoque

---
+++ src/main/java/br/com/coffeebreak/model/estoque/Estoque.java
@@ -0,0 +1,28 @@
+package br.com.coffeebreak.model.estoque;
+
+import br.com.coffeebreak.model.produto.Produto;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "estoque")
+public class Estoque {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private Integer quantidade;
+
+
+}
