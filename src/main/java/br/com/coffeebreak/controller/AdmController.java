30fb2f1ea3d29eb269bfb31251b0b850fb2d8d6b
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-04-16 00:26:12 -0300

    feat: cria rota para login

--- src/main/java/br/com/coffeebreak/controller/AdmController.java
+++ src/main/java/br/com/coffeebreak/controller/AdmController.java
@@ -1,6 +1,7 @@
 package br.com.coffeebreak.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,5 +21,10 @@ public class AdmController {
     @RequestMapping("/home")
     public String home(){
         return "/coffeebreak/home";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "/coffeebreak/login";
     }
 }
