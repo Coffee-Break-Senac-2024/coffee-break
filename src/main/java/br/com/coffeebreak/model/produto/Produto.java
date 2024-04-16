423e1d81b29ba1a84e88385fb0fc16e2e09db572
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-04-16 02:15:23 -0300

    feat: model produto com relacionamento OneToMany(um produto usa varios ingredientes do estoque)

---
+++ src/main/java/br/com/coffeebreak/model/produto/Produto.java
@@ -0,0 +1,35 @@
+package br.com.coffeebreak.model.produto;
+
+import br.com.coffeebreak.model.estoque.Estoque;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "produto")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private Integer quantidadeUtilizada;
+
+    @Column(nullable = false)
+    private Double preco;
+
+    @OneToMany
+    @JoinColumn(name = "id_produto")
+    private List<Estoque> estoqueList;
+
+}
