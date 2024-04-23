31006ecd89970ced017314a06c8b538041fa1bc7
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-04-22 22:37:13 -0300

    feat: funcionalidade de criacao de produto e listagem

--- src/main/java/br/com/coffeebreak/controller/StockController.java
+++ src/main/java/br/com/coffeebreak/controller/StockController.java
@@ -1,10 +1,17 @@
 package br.com.coffeebreak.controller;
 
+import br.com.coffeebreak.model.estoque.Estoque;
 import br.com.coffeebreak.model.produto.Produto;
+import br.com.coffeebreak.repositories.EstoqueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,10 +20,33 @@ import java.util.List;
 @RequestMapping("/administrator/stock")
 public class StockController {
 
-    @GetMapping
+    @Autowired
+    private EstoqueRepository estoqueRepository;
+
+    @GetMapping("/")
     public ModelAndView index(){
         ModelAndView mv = new ModelAndView("adm/stock/create");
         return mv;
+    }
+
+    @PostMapping("/create")
+    public ModelAndView cadastrarIngrediente(@ModelAttribute Estoque estoque, RedirectAttributes redirectAttributes) {
+        try {
+            Estoque saved = this.estoqueRepository.save(estoque);
+            redirectAttributes.addFlashAttribute("message", "Ingrediente cadastrado com sucesso!");
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("message", "Erro ao cadastrar ingrediente: ");
+        }
+        return new ModelAndView("redirect:/administrator/stock/");
+    }
+
+    @GetMapping("/ingredientes")
+    public ModelAndView listarEstoque() {
+        ModelAndView mv = new ModelAndView("adm/stock/list");
+        List<Estoque> ingredientes = this.estoqueRepository.findAll();
+        mv.addObject("ingredientes", ingredientes);
+        return mv;
+
     }
 
 }
