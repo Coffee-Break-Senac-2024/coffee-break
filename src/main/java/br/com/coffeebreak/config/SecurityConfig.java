b3cb2d87a265f09a94aa9d836a8552373b60ca4b
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-05-07 00:55:40 -0300

    feat: criacao de jwt

---
+++ src/main/java/br/com/coffeebreak/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package br.com.coffeebreak.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(csrf -> csrf.disable());
+        httpSecurity.authorizeHttpRequests(auth -> {
+           auth.anyRequest().permitAll();
+        });
+
+        return httpSecurity.build();
+    }
+
+
+}
