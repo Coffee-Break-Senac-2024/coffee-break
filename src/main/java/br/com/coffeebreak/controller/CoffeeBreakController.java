556fbe1df5542b1926028747df5f54985c4a921f
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-05-14 17:18:00 -0300

    fix

--- src/main/java/br/com/coffeebreak/controller/CoffeeBreakController.java
+++ src/main/java/br/com/coffeebreak/controller/CoffeeBreakController.java
@@ -1,16 +1,12 @@
 package br.com.coffeebreak.controller;
 
-<<<<<<< HEAD
 import br.com.coffeebreak.dto.UserDTO;
-=======
-import br.com.coffeebreak.controller.produto.ProductController;
 import br.com.coffeebreak.model.produto.Produto;
 import br.com.coffeebreak.service.produto.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
->>>>>>> 489801ef408f07b1307511d6e6458f3b4b7cb554
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/br/com/coffeebreak/model/funcionario/Funcionario.java
+++ src/main/java/br/com/coffeebreak/model/funcionario/Funcionario.java
@@ -35,12 +35,9 @@ public class Funcionario implements UserDetails {
     private String nome;
 
     @Column(nullable = false)
-<<<<<<< HEAD
     @NotNull(message = "O campo tipo de funcinário é obrigatótio.")
     @Enumerated(EnumType.STRING)
-=======
     @NotNull(message = Mensagem.FORMULARIO_VALIDACAO_TIPO_FUNCIONARIO)
->>>>>>> 489801ef408f07b1307511d6e6458f3b4b7cb554
     private TipoFuncionario tipoFuncionario;
 
     @Column(unique = true, nullable = false)
@@ -48,12 +45,6 @@ public class Funcionario implements UserDetails {
     private String email;
 
     @Column(nullable = false)
-<<<<<<< HEAD
-    @NotEmpty(message = "O campo nome é obrigatótio")
-    private String senha;
-
-    @Column(nullable = false)
-=======
     @NotEmpty(message = Mensagem.FORMULARIO_VALIDACAO_SENHA)
     @Size(max = 14, min = 8, message = Mensagem.FORMULARIO_VALIDACAO_SENHA_TAMANHO)
     private String senha;
@@ -61,7 +52,6 @@ public class Funcionario implements UserDetails {
     @Column(nullable = false)
     @NotNull(message = Mensagem.FORMULARIO_VALIDACAO_DATA)
     @DateTimeFormat(pattern = "yyyy-MM-dd")
->>>>>>> 489801ef408f07b1307511d6e6458f3b4b7cb554
     private LocalDateTime entrada;
 
 
