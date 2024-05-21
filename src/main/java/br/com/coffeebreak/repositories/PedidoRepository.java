ef6855c55019fcf89733e0bb1024c05431ecbae3
Lauriano <laurianocarlos@hotmail.com>
2024-05-21 00:27:53 -0300

    Busca por pedidos em andamento para clientes

--- src/main/java/br/com/coffeebreak/repositories/PedidoRepository.java
+++ src/main/java/br/com/coffeebreak/repositories/PedidoRepository.java
@@ -1,5 +1,6 @@
 package br.com.coffeebreak.repositories;
 
+import br.com.coffeebreak.enums.SituacaoPedido;
 import br.com.coffeebreak.model.pedido.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,6 +9,7 @@ import java.util.List;
 
 public interface PedidoRepository extends JpaRepository<Pedido, String> {
 
-    @Query("SELECT situacao FROM pedido situacao WHERE situacao.situacao = 'EM_ANDAMENTO'")
-    List<Pedido> getPedidos();
+    @Query("SELECT p FROM pedido p WHERE p.cliente.id = :clienteId AND p.situacao = :situacao")
+    List<Pedido> getPedidosPorClienteStatus(String clienteId, String situacao);
+
 }
