96fd639f15bf0f968d7aa6939e8ae9bfe5f1bf0d
Lauriano <laurianocarlos@hotmail.com>
2024-05-20 23:31:28 -0300

    Adicionado usuario logado ao pedido

--- src/main/java/br/com/coffeebreak/repositories/ClienteRepository.java
+++ src/main/java/br/com/coffeebreak/repositories/ClienteRepository.java
@@ -3,10 +3,7 @@ package br.com.coffeebreak.repositories;
 import br.com.coffeebreak.model.cliente.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.security.core.userdetails.UserDetails;
 
-
-import java.lang.ref.Cleaner;
 import java.util.Optional;
 
 
@@ -15,9 +12,7 @@ public interface ClienteRepository extends JpaRepository<Cliente,String> {
     @Query(value="SELECT c FROM cliente c WHERE c.cpf = ?1")
     Optional<Cliente> findByCpf(String cpf);
 
-    @Query(value="SELECT c FROM cliente c WHERE c.email = ?1")
     Optional<Cliente> findByEmail(String email);
-    Cliente findClienteByEmailIgnoreCase(String email);
 
     Optional<Cliente> findByCpfOrEmailOrTelefone(String cpf, String email, String telefone);
 
