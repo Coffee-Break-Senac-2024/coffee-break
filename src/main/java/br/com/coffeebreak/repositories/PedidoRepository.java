0df425d998ab56a0e8bce3fc689ee4f7bd39698f
Lauriano <laurianocarlos@hotmail.com>
2024-05-22 09:38:29 -0300

    Alteração listagem de produto spor status/formataÃcao valor do pedido

--- src/main/java/br/com/coffeebreak/repositories/PedidoRepository.java
+++ src/main/java/br/com/coffeebreak/repositories/PedidoRepository.java
@@ -1,6 +1,5 @@
 package br.com.coffeebreak.repositories;
 
-import br.com.coffeebreak.enums.SituacaoPedido;
 import br.com.coffeebreak.model.pedido.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,5 +10,8 @@ public interface PedidoRepository extends JpaRepository<Pedido, String> {
 
     @Query("SELECT p FROM pedido p WHERE p.cliente.id = :clienteId AND p.situacao = :situacao")
     List<Pedido> getPedidosPorClienteStatus(String clienteId, String situacao);
+
+    @Query("SELECT p FROM pedido p WHERE  p.situacao = :situacao")
+    List<Pedido> getPedidosPorAndamento(String situacao);
 
 }
