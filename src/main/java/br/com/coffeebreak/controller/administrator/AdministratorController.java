c5d05b8d779a236858eb5990ef3e47447d61b037
Lauriano <laurianocarlos@hotmail.com>
2024-05-22 12:22:53 -0300

    Ajuste nos modais para incluir modal na tela de Pedido

--- src/main/java/br/com/coffeebreak/controller/administrator/AdministratorController.java
+++ src/main/java/br/com/coffeebreak/controller/administrator/AdministratorController.java
@@ -7,10 +7,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import br.com.coffeebreak.service.pedido.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -30,9 +27,8 @@ public class AdministratorController {
         return mv;
     }
 
-    @PostMapping("/finalizar-pedido")
-    public ModelAndView finalizarPedido(@RequestParam("id") String id){
-        System.out.println("ID: " + id);
+    @PostMapping("/finalizar-pedido/{id}")
+    public ModelAndView finalizarPedido(@PathVariable("id") String id){
         service.FinalizarPedido(id);
         return new ModelAndView("redirect:/administrator");
 
